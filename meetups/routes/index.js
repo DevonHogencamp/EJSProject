638df0763a15164feadae5ecb1f38040e5bc26da
Devon Hogencamp <devhogen1@gmail.com>
2017-02-15 15:28:13 -0700

    social media icons and speaker list

--- meetups/routes/index.js
+++ meetups/routes/index.js
@@ -1,10 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var appdata = require('../data.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    var myArtwork = [];
+    var myArtists = [];
+
+    myArtists = appdata.speakers;
+
+    appdata.speakers.forEach(function (item) {
+        myArtwork = myArtwork.concat(item.artwork);
+    });
+
     res.render('index', {
-        title: 'Express'
+        title: 'Home',
+        artwork: myArtwork,
+        artists: myArtists
+    });
+});
+
+/* GET speakers page */
+router.get('/speakers', function(req, res, next) {
+    var myArtwork = [];
+    appdata.speakers.forEach(function (item) {
+        myArtwork = myArtwork.concat(item.artwork);
+    });
+
+    res.render('speakers', {
+        title: 'Speakers',
+        artwork: myArtwork
+    });
+});
+
+/* GET speakers detail page */
+router.get('/speakers/:speakerid', function(req, res, next) {
+    var myArtwork = [];
+    appdata.speakers.forEach(function (item) {
+        if (item.shortname == req.params.speakerid) {
+            marArtists.push(item);
+            myArtwork = myArtwork.concat(item.artwork);
+        }
+    });
+
+    res.render('speakers', {
+        title: 'Speakers',
+        artwork: myArtwork,
+        artists: myArtists
     });
 });
 
