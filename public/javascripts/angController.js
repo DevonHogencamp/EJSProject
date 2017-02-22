df3b36e306c32689d6c6c949bcbae60a7787618f
Devon Hogencamp <devhogen1@gmail.com>
2017-02-22 15:31:31 -0700

    working on class for body changing based on route

--- public/javascripts/angController.js
+++ public/javascripts/angController.js
@@ -1,7 +1,11 @@
 // Create a new module
-var artistControllers = angular.module('artistControllers', []);
+var artistControllers = angular.module('artistControllers', [
+    'ngRoute'
+]);
 
-artistControllers.controller('listController', ['$scope', '$http', function listController($scope, $http) {
+artistControllers.controller('listController', ['$scope', '$http', function listController($scope, $http, $routeParams) {
+    $scope.params = $routeParams;
+
     $http.get('/data.json').then(function(res){
         $scope.artists = res.data.speakers;
         $scope.artistOrder = "name";
